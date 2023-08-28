1ed7e1776daba86638e3cca8e2017d8459a8887d
Dmytro2708 <130864203+Dmytro2708@users.noreply.github.com>
2023-08-29 01:11:23 +0300

    add files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description}) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`);
+
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+gallery.addEventListener('click', onClick);
+
+
+function onClick(evt) {
+    evt.preventDefault();
+    const { target } = evt
+    if(!target.classList.contains("gallery__image")) {
+        return;
+    }
+    const gallerySource = target.dataset.source;
+    const galleryAttributeAlt = target.getAttribute("alt");
+    const instance = basicLightbox.create(`
+    <img
+    src="${gallerySource}"
+    alt="${galleryAttributeAlt}"
+    />`)
+    instance.show()
+
+
+    gallery.addEventListener('keydown', handleKeyPress);
+
+    function handleKeyPress(evt) {
+    if (evt.key === 'Escape') {
+      instance.close();
+      gallery.removeEventListener('keydown', handleKeyPress);
+    }
+
+}}
